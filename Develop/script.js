2cd13ef9e33db59cbd991775328f9a5de4dfb185
Max <max.belenchuk@gmail.com>
2022-11-10 17:25:48 -0400

    Added variables for each criteria that will be inputed by user

---
+++ Develop/script.js
@@ -0,0 +1,33 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacter = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|"]
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+var confirmSpecialCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumber;
+var confirmLength;
+
+
+
+
+function writePassword() {
+  
+  
+  
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword);
